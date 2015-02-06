0f16e2076f5da8cf7f7c474c34b39a8a12141764
Yannick Rödl <yannick.roedl@gmx.de>
2015-02-06 10:32:18 +0100

    Refs #16 some context layout improvements.

--- Android/Shopr/src/main/java/com/uwetrottmann/shopr/context/ui/ContextActivity.java
+++ Android/Shopr/src/main/java/com/uwetrottmann/shopr/context/ui/ContextActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.uwetrottmann.shopr.R;
 import com.uwetrottmann.shopr.eval.Statistics;
@@ -42,6 +43,7 @@ public class ContextActivity extends Activity implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
+        Toast.makeText(this, R.string.startRecommendation, Toast.LENGTH_LONG).show();
         Intent i = new Intent(this, MainActivity.class);
         startActivity(i);
         Statistics.get().startRecommendingItems(); // Set this, which states, that the real recommendation process begins.
